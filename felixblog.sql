80cbe0670217bd68d251ef174ae31c392055afee
unknown <lrq1988@gmail.com>
2013-11-05 17:29:09 +0800

    sql

---
+++ felixblog.sql
@@ -0,0 +1,71 @@
+/*
+SQLyog Ultimate v8.32 
+MySQL - 5.1.46-community : Database - blogsite
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`blogsite` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `blogsite`;
+
+/*Table structure for table `blog` */
+
+DROP TABLE IF EXISTS `blog`;
+
+CREATE TABLE `blog` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) DEFAULT NULL,
+  `tag` varchar(100) DEFAULT NULL,
+  `summary` varchar(256) DEFAULT NULL,
+  `content` text,
+  `postdate` varchar(20) DEFAULT NULL,
+  `uuid` varchar(36) DEFAULT NULL,
+  `filepath` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `blog` */
+
+/*Table structure for table `tag` */
+
+DROP TABLE IF EXISTS `tag`;
+
+CREATE TABLE `tag` (
+  `id` int(11) DEFAULT NULL,
+  `tagname` varchar(50) DEFAULT NULL,
+  `ext` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `tag` */
+
+insert  into `tag`(`id`,`tagname`,`ext`) values (1,'计算机',NULL),(2,'JAVA',NULL),(3,'CLOJURE',NULL),(4,'扯淡',NULL),(5,'编程',NULL);
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) DEFAULT NULL,
+  `password` varchar(40) DEFAULT NULL,
+  `level` varchar(10) DEFAULT NULL,
+  `limit` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `user` */
+
+insert  into `user`(`id`,`username`,`password`,`level`,`limit`) values (1,'admin','admin',NULL,NULL);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
